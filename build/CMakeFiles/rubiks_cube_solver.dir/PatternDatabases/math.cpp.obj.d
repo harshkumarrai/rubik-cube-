077e5785b050a95e5a8b4675bd7efb05fafca724
harsh <hkrai1372@gmail.com>
2024-11-18 02:20:34 +0530

    Initial commit

---
+++ build/CMakeFiles/rubiks_cube_solver.dir/PatternDatabases/math.cpp.obj.d
@@ -0,0 +1,12 @@
+CMakeFiles/rubiks_cube_solver.dir/PatternDatabases/math.cpp.obj: \
+ C:\Users\hkrai\Downloads\rubiks-cube-solver-main-main\rubiks-cube-solver-main-main\rubiks-cube-solver-main\PatternDatabases\math.cpp \
+ C:\Users\hkrai\Downloads\rubiks-cube-solver-main-main\rubiks-cube-solver-main-main\rubiks-cube-solver-main\PatternDatabases\math.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\cstdint \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\mingw32\bits\c++config.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\mingw32\bits\os_defines.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\mingw32\bits\cpu_defines.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stdint.h \
+ c:\mingw\include\stdint.h c:\mingw\include\_mingw.h \
+ c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
+ c:\mingw\include\sdkddkver.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h
